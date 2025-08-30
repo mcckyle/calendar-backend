87d71360289d579e158a7d9fa54d3db217abe1d4
Kyle McColgan <kdmbus21@gmail.com>
2025-08-29 20:10:51 -0500

    EventProxyController.java: Make the endpoint return a ResponseEntity<String>, instead of just String.

--- src/main/java/com/mcckyle/eventproxy/controller/EventProxyController.java
+++ src/main/java/com/mcckyle/eventproxy/controller/EventProxyController.java
@@ -1,6 +1,8 @@
 package com.mcckyle.eventproxy.controller;
 
 import com.mcckyle.eventproxy.service.EventService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +20,12 @@ public class EventProxyController
     }
 
     @GetMapping("/events")
-    public String getEvents(@RequestParam String city, @RequestParam String start, @RequestParam String end)
+    public ResponseEntity<String> getEvents(@RequestParam String city, @RequestParam String start, @RequestParam String end)
     {
-        return eventService.fetchEvents(city, start, end);
+        String events = eventService.fetchEvents(city, start, end);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(events);
     }
 }
