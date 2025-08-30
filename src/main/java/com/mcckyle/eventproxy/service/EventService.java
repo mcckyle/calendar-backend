abd85a861965e19d1a8cc35f3ec309348dab7694
Kyle McColgan <kdmbus21@gmail.com>
2025-08-29 20:54:45 -0500

    EventService.java: Remove encoding of the city parameter to avoid double encoding issues.

--- src/main/java/com/mcckyle/eventproxy/service/EventService.java
+++ src/main/java/com/mcckyle/eventproxy/service/EventService.java
@@ -7,9 +7,6 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriUtils;
-
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class EventService
@@ -21,11 +18,9 @@ public class EventService
 
     public String fetchEvents(String city, String start, String end)
     {
-        String encodedCity = UriUtils.encode(city, StandardCharsets.UTF_8);
-
         String ticketmasterUrl = String.format(
                 "https://app.ticketmaster.com/discovery/v2/events.json?apikey=%s&city=%s&startDateTime=%sT00:00:00Z&endDateTime=%sT23:59:59Z",
-                apiKey, encodedCity, start, end
+                apiKey, city, start, end
         );
 
         try
